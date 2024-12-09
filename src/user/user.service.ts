652ca565e8cac41bee4e9ce83adae6a3a1ffc3f6
Choi-jeonghoon <jeongssi94@gmail.com>
2024-12-09 20:16:07 +0900

    feat: DB 연결

---
+++ src/user/user.service.ts
@@ -0,0 +1,54 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
+
+  create(createUserDto: CreateUserDto) {
+    return this.userRepository.save(createUserDto);
+  }
+
+  findAll() {
+    return this.userRepository.find();
+  }
+
+  async findOne(id: number) {
+    const user = await this.userRepository.findOne({
+      where: { id },
+    });
+    if (!user) {
+      throw new NotFoundException('찾고자하는 id 가 없습니다.');
+    }
+
+    return user;
+  }
+
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    const user = await this.userRepository.findOne({
+      where: { id },
+    });
+    if (!user) {
+      throw new NotFoundException('찾고자하는 id 가 없습니다.');
+    }
+
+    await this.userRepository.update({ id }, updateUserDto);
+
+    return (
+      this,
+      this.userRepository.findOne({
+        where: { id },
+      })
+    );
+  }
+
+  remove(id: number) {
+    return this.userRepository.delete(id);
+  }
+}
